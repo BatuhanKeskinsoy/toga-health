80e67e4ba2e88f420317458f4c20ac28303ad315
Batuhan Keskinsoy <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2025-10-07 16:06:34 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TOGA Health",
+  description: "TOGA Health",
+  robots: "noindex, nofollow",
+};
+
 export default function RootLayout({
   children,
 }: {
