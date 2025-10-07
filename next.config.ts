a10e21e1a39bfc0172dea7c49ae6e80d3ce68086
Batuhan Keskinsoy <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2025-10-07 14:29:17 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   devIndicators: false,
   reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     remotePatterns: [
